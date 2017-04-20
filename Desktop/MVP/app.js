68eb3d47a57c63997e180d3a0972e9fc52b73368
BettyBeiZhang <beizhang18@yahoo.com>
2017-04-20 00:41:08 -0700

    add handle connection due to mySql server down

--- Desktop/MVP/app.js
+++ Desktop/MVP/app.js
@@ -25,22 +25,47 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //
 // });
 //
-var dbConnection = mysql.createConnection({
+//dbConnection.connect();
+
+var db_config = {
 	host:'us-cdbr-iron-east-03.cleardb.net',
 	user: 'bf4ab44c1b29c7',
 	password: "8516876a",
 	database: 'heroku_feefe01914f3eb6'
-});
+};
 
-dbConnection.connect();
 
+var connection;
+
+function handleDisconnect() {
+    console.log('1. connecting to db:');
+    connection = mysql.createConnection(db_config); // Recreate the connection, since the old one cannot be reused.
+
+    connection.connect(function(err) {              	// The server is either down
+        if (err) {                                     // or restarting (takes a while sometimes).
+            console.log('2. error when connecting to db:', err);
+            setTimeout(handleDisconnect, 1000); // We introduce a delay before attempting to reconnect,
+        }                                     	// to avoid a hot loop, and to allow our node script to
+    });                                     	// process asynchronous requests in the meantime.
+    											// If you're also serving http, display a 503 error.
+    connection.on('error', function(err) {
+        console.log('3. db error', err);
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') { 	// Connection to the MySQL server is usually
+            handleDisconnect();                      	// lost due to either server restart, or a
+        } else {                                      	// connnection idle timeout (the wait_timeout
+            throw err;                                  // server variable configures this)
+        }
+    });
+}
+
+handleDisconnect();
 
 
 app.post('/takeNote', function(request, response) {
 	var params = [request.body.note.titleArea, request.body.note.textArea,request.body.note.date];
 	var queryString = 'INSERT INTO Note (EntryTitle, EntryText, YMD) values (?, ?, ?)';
      //console.log(request.body.note.titleArea, request.body.note.date);
-	 dbConnection.query(queryString, params, function(err,rows,fields) {
+	 connection.query(queryString, params, function(err,rows,fields) {
 		response.json(rows);
 	});
 
@@ -49,12 +74,13 @@ app.post('/takeNote', function(request, response) {
 app.post('/rendernotes', function(request, response) {
     var queryString = `select * From Note where YMD="${request.body.date}"`;
    console.log('queryString', queryString);
-    dbConnection.query(queryString, function(err, results, fields) {
+    connection.query(queryString, function(err, results, fields) {
     	if(err) console.log(err);
    	   response.send(results);
     });
 });
 
+//for testing heroku run local web
 app.get('/cool', function(request, response) {
 	response.send(cool());
 });
